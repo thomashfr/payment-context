3db19414cebedb63a281cfd71215fa0333ddf56e
Thomas Henrique Fideles Ribeiro <thomas.henrique@sinqia.com.br>
2021-03-18 00:21:39 -0300

    feat:comit inicial

---
+++ PaymentContext.Domain/Entities/Subscription.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace PaymentContext.Domain.Entities
+{
+  public class Subscription
+  {
+    public DateTime CreateDate { get; set; }
+    public DateTime LastUpdateDate { get; set; }
+    public DateTime? ExpireDate { get; set; }
+    public bool Active { get; set; }
+  }
+}
